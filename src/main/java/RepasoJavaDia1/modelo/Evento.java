25165ad60856cb9cccd3abc97848a964aa05e674
Maury <mramesp607@g.educaand.es>
2026-09-23 22:53:21 +0200

    Completa clase Evento: equals/hashCode, getPorcentajeOcupacion, modificaEstado, calcularCosteBase abstracto y toString

--- src/main/java/RepasoJavaDia1/modelo/Evento.java
+++ src/main/java/RepasoJavaDia1/modelo/Evento.java
@@ -1,6 +1,8 @@
 package RepasoJavaDia1.modelo;
 
 import java.time.LocalDate;
+import java.util.Objects;
+
 import RepasoJavaDia1.exceptions.CraftersException;
 
 public abstract class Evento {
@@ -31,4 +33,71 @@ public abstract class Evento {
 		this.id = ++contID;// para cada id creado con el objeto, se suma +1 al contador cuyo valor es 0
 		this.estado = Estado.PENDIENTE;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Integer.valueOf(id));
+	}
+
+	// Punto 3 ejercicio .equals
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Evento other = (Evento) obj;
+		return id == other.id;
+	}
+
+	// Metodo getPorcentajeOcupacion sin parámetro
+	public double getPorcentajeOcupacion() {
+		double porcentaje = (double) numEntradasVendidas / capMaxAsistentes * 100;
+		return porcentaje;
+	}
+
+	// Metodo getPorcentajeOcupacion con parámetro
+	public double getPorcentajeOcupacion(int numEntradas) {
+		double porcentajeResultado = (double) (numEntradasVendidas + numEntradas) / capMaxAsistentes * 100;
+		return porcentajeResultado;
+	}
+
+	// Punto 5 del ejercicio NO HE PODIDO HACERLO SOLO!!!
+	public void modificaEstado(Estado nuevo) throws CraftersException {
+
+		switch (this.estado) {
+
+		case PENDIENTE:
+			if (nuevo != Estado.PROGRAMADO && nuevo != Estado.CANCELADO && nuevo != Estado.FINALIZADO) {
+				throw new CraftersException("No es posible pasar al estado: " + nuevo);
+			}
+			break;
+
+		case APLAZADO:
+			if (nuevo != Estado.PROGRAMADO) {
+				throw new CraftersException("No es posible pasar al estado: " + nuevo);
+			}
+			break;
+
+		default:
+			// PROGRAMADO, CANCELADO o FINALIZADO como estado actual: nunca se puede avanzar
+			throw new CraftersException("No es posible pasar al estado: " + nuevo);
+		}
+
+		// Si el switch no lanzó ninguna excepción, la transición es válida: se aplica
+		// el cambio
+		this.estado = nuevo;
+	}
+
+	// Método abstracto que implica a las clases hjjas que es donde se implementa
+	public abstract double calcularCosteBase();
+
+	// To String
+	@Override
+	public String toString() {
+		return "Evento [id=" + id + ", nombre=" + nombre + ", fecha=" + fecha + ", estado=" + estado + "]";
+	}
+
 }
